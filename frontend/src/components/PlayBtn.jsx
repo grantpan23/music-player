450dc67aeb988b40150dd89a60cf196d995e2e09
grantpanman <gpan7@uwo.ca>
2023-01-06 14:41:51 -0500

    Minor

--- frontend/src/components/PlayBtn.jsx
+++ frontend/src/components/PlayBtn.jsx
@@ -1,7 +1,6 @@
 
 export default function PlayBtn(props){
     const search=props.searchKey.replace(/ /g,"+")
-    console.log(search)
 
     return(
         <a href={`https://www.youtube.com/results?search_query=${search}`} target="_blank">Play Btn</a>
